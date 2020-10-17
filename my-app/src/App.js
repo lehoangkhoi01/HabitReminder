e69e87293a3eaedc48ae31c89a86136d570b9f07
unknown <khoi@gmail.com>
2020-10-17 17:46:30 +0700

    create todo list

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,14 @@
-import React from 'react'
+import React from 'react';
 import './App.css';
+import 'antd/dist/antd.css';
+import TodoForm from './Components/TodoForm';
+import TodoList from './Components/TodoList';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="reminder-app">
+      <TodoList/>
     </div>
   );
 }
