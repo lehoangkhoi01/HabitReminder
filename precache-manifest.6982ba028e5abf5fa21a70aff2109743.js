4d26e1b7b1a3b683a9477fe391696b3d1741b646
unknown <khoi@gmail.com>
2020-10-22 22:10:50 +0700

    Updates

---
+++ precache-manifest.6982ba028e5abf5fa21a70aff2109743.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74e84b66f63f2ab86fd4279c559e1dc7",
+    "url": "/HabitReminder/index.html"
+  },
+  {
+    "revision": "7265ea2320d83480fee7",
+    "url": "/HabitReminder/static/css/2.e10332d9.chunk.css"
+  },
+  {
+    "revision": "f4e85eccd90531cf6d7a",
+    "url": "/HabitReminder/static/css/main.aed555c9.chunk.css"
+  },
+  {
+    "revision": "7265ea2320d83480fee7",
+    "url": "/HabitReminder/static/js/2.8188f282.chunk.js"
+  },
+  {
+    "revision": "6083d9c769f39b252a49c0c719a4b64c",
+    "url": "/HabitReminder/static/js/2.8188f282.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4e85eccd90531cf6d7a",
+    "url": "/HabitReminder/static/js/main.3ee3edb4.chunk.js"
+  },
+  {
+    "revision": "969b5c9d89b9233519b9",
+    "url": "/HabitReminder/static/js/runtime-main.cf5ac7a3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/HabitReminder/my-app/precache-manifest.332532382d75279c477fda6a4bfe4dfd.js"
+  "/HabitReminder/precache-manifest.6982ba028e5abf5fa21a70aff2109743.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/HabitReminder/my-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/HabitReminder/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
